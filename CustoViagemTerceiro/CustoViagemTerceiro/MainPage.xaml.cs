b211dd42c5e13c1c7dbc40905d5cf67960ef29f5
TIAGO SILVA <frost.tiago@gmail.com>
2021-04-30 12:25:25 -0300

    Adicionar arquivos de projeto.

---
+++ CustoViagemTerceiro/CustoViagemTerceiro/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustoViagemTerceiro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new MostraListaPedagios());
+
+            } catch(Exception ex)
+            {
+                DisplayAlert("Oooops", ex.Message, "OK");
+
+            }
+        }
+
+        private async void Button_Limpar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                bool confirm = await DisplayAlert("Tem Certeza?", "Limpar todos os campos?", "Sim", "Não");
+
+                if(confirm)
+                {              
+                    txt_consumo.Text = "";
+                    txt_destino.Text = "";
+                    txt_distancia.Text = "";
+                    txt_localizacao.Text = "";
+                    txt_origem.Text = "";
+                    txt_valor_combustivel.Text = "";
+                    txt_valor_pedagio.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Oooops", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ CustoViagemTerceiro/CustoViagemTerceiro/MostraListaPedagios.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustoViagemTerceiro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MostraListaPedagios : ContentPage
+    {
+        public MostraListaPedagios()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
